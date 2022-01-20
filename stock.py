4d0dfda914d53e532c992154c817534e15f4c0a9
JLucasAlencar <alencarj2021@gmail.com>
2022-01-20 09:55:28 -0300

    update

--- stock.py
+++ stock.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
     Example:
     name: Saturn V
     Payload price: R$2000000000,00
+    Quantity: 10
 '''
 
 @dataclass
@@ -13,15 +14,6 @@ class product:
     name: str
     payload_price: float
     quantity: int
-
-saturn_V = product('Saturn V', 200000000, 10)
-falcon_9 = product('Falcon 9', 1000000, 20)
-ariane_5 = product('Ariane 5', 10000000, 8)
-
-
-listOfProducts = [saturn_V, falcon_9]
-
-print(listOfProducts)
 
 class stock:
     name: str
@@ -31,7 +23,7 @@ class stock:
         self.name = name
         self.payloadPrice = payloadPrice
         self.quantity = quantity
-        listOfProducts.append()
+        
     #def show_products(self):
         
     #def buy_item(self, quantity):
@@ -40,6 +32,12 @@ new_product.name = str(input('Digite o nome do foguete: '))
 new_product.payloadPrice = float(input('Digite o preço da payload'))
 new_product.quantity = int(input('Qunatidade disponível do produto'))
 
+
+with open('stock.txt') as fo:
+    for line in fo:
+        print(line.split('#')[2])
+a = int(line.split('#')[2])
+print(a+1)
 
 
 
